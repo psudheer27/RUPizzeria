0d623a2ebe08a2e92e56389c7223359174160b5d
pranav sudheer <pranav.sudheer19@gmail.com>
2024-11-16 19:17:06 -0500

    Setup

--- src/main/java/pizzeria/BBQChicken.java
+++ src/main/java/pizzeria/BBQChicken.java
@@ -1,6 +1,14 @@
 package pizzeria;
 
 public class BBQChicken extends Pizza{
+
+    public BBQChicken() {
+        addTopping(Topping.BBQChicken);
+        addTopping(Topping.GreenPepper);
+        addTopping(Topping.Provolone);
+        addTopping(Topping.Cheddar);
+    }
+
     @Override
     public double price() {
         return 0;
--- src/main/java/pizzeria/Meatzza.java
+++ src/main/java/pizzeria/Meatzza.java
@@ -2,6 +2,13 @@ package pizzeria;
 
 public class Meatzza extends Pizza{
 
+    public Meatzza() {
+        addTopping(Topping.Sausage);
+        addTopping(Topping.Pepperoni);
+        addTopping(Topping.Ham);
+        addTopping(Topping.Beef);
+    }
+
     @Override
     public double price() {
         return 0;
